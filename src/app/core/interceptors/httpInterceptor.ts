d2e8872b2971f24f4f3faf8b201c4338f72793ba
yksolanki9 <yashsolanki1709@gmail.com>
2022-12-25 18:40:25 +0530

    Remove interceptor check for unsecure urls

--- src/app/core/interceptors/httpInterceptor.ts
+++ src/app/core/interceptors/httpInterceptor.ts
@@ -11,17 +11,26 @@ import { Preferences } from '@capacitor/preferences';
 
 @Injectable()
 export class HttpReqInterceptor implements HttpInterceptor {
+  secureUrl(url: string) {
+    const unsecureUrls = ['login', 'register'];
+    return !unsecureUrls.some((unsecureUrl) => url.includes(unsecureUrl));
+  }
+
   intercept(
     req: HttpRequest<any>,
     next: HttpHandler
   ): Observable<HttpEvent<any>> {
-    return from(Preferences.get({ key: 'ACCESS_TOKEN' })).pipe(
-      switchMap((accessToken) => {
-        const modifiedRequest = req.clone({
-          headers: req.headers.append('x-access-token', accessToken.value),
-        });
-        return next.handle(modifiedRequest);
-      })
-    );
+    if (this.secureUrl(req.url)) {
+      return from(Preferences.get({ key: 'ACCESS_TOKEN' })).pipe(
+        switchMap((accessToken) => {
+          const modifiedRequest = req.clone({
+            headers: req.headers.append('x-access-token', accessToken.value),
+          });
+          return next.handle(modifiedRequest);
+        })
+      );
+    } else {
+      return next.handle(req);
+    }
   }
 }
