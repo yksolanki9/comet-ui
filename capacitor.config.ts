83478be7e5ca80e0e5098b5ba12c467fe2c4f9a2
yksolanki9 <yashsolanki1709@gmail.com>
2022-12-26 23:09:25 +0530

    Rename ionic package in files

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,10 +1,10 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'com.ionic.comet_daily_journal',
   appName: 'comet',
   webDir: 'www',
-  bundledWebRuntime: false
+  bundledWebRuntime: false,
 };
 
 export default config;
