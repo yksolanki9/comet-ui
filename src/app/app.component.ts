e62f7dc4e0da396c90a45b7f845dc7e11dfd0551
yksolanki9 <yashsolanki1709@gmail.com>
2023-02-25 14:12:23 +0530

    Add notif icon and test notification

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { LocalNotifications } from '@capacitor/local-notifications';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,19 @@ import { Component } from '@angular/core';
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
-  constructor() {}
+  constructor() {
+    this.scheduleNotifications();
+  }
+
+  private async scheduleNotifications() {
+    await LocalNotifications.schedule({
+      notifications: [
+        {
+          id: 1,
+          title: 'Testing notifications',
+          body: 'Please write about your day',
+        },
+      ],
+    });
+  }
 }
