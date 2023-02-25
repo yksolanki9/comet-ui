03822224f89c5d6fcc3dfa4e3d94b6ffdd98c560
yksolanki9 <yashsolanki1709@gmail.com>
2023-02-25 16:38:24 +0530

    Create reminder service and integrate it with login/register page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { LocalNotifications } from '@capacitor/local-notifications';
 
 @Component({
   selector: 'app-root',
@@ -7,19 +6,5 @@ import { LocalNotifications } from '@capacitor/local-notifications';
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
-  constructor() {
-    this.scheduleNotifications();
-  }
-
-  private async scheduleNotifications() {
-    await LocalNotifications.schedule({
-      notifications: [
-        {
-          id: 1,
-          title: 'Testing notifications',
-          body: 'Please write about your day',
-        },
-      ],
-    });
-  }
+  constructor() {}
 }
